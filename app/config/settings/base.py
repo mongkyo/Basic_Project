fa37a243fc402273d7108836f969ad71235dfc98
mongkyo <dreamong91@gmail.com>
2019-05-14 20:34:28 +0900

    [add] secrets key

--- app/config/settings/base.py
+++ app/config/settings/base.py
@@ -9,18 +9,20 @@ https://docs.djangoproject.com/en/2.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
-
+import json
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+ROOT_DIR = os.path.dirname(BASE_DIR)
 
+SECRET_ROOT = os.path.join(ROOT_DIR, '.secrets')
+base_secrets = json.load(open(os.path.join(SECRET_ROOT, 'base.json')))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '&oc^jicpgn0_@6pdt)unwfwtkys3&a4fxxwiw%e6$97kxwzje8'
+SECRET_KEY = "&oc^jicpgn0_@6pdt)unwfwtkys3&a4fxxwiw%e6$97kxwzje8"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
